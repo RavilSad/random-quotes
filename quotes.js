f5cb196c767f4a51d4ed0ae5fb2521ff2134e0e9
Sadykov Ravil <mr.sadykov.ravil@gmail.com>
2025-04-12 00:07:56 +0300

    show favorites qoutes

---
+++ quotes.js
@@ -0,0 +1,44 @@
+const quotes = [
+  {
+    text: "Believe you can and you're halfway there.",
+    author: 'Theodore Roosevelt',
+  },
+  {
+    text: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+  },
+  {
+    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    author: 'Winston Churchill',
+  },
+  {
+    text: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    text: 'You miss 100% of the shots you don’t take.',
+    author: 'Wayne Gretzky',
+  },
+  {
+    text: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+  },
+  {
+    text: 'The best way to predict the future is to create it.',
+    author: 'Peter Drucker',
+  },
+  {
+    text: 'Do what you can, with what you have, where you are.',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    text: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: 'Thomas Edison',
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    author: 'John Lennon',
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,67 +1,56 @@
-const quotes = [
-  {
-    text: "Believe you can and you're halfway there.",
-    author: 'Theodore Roosevelt',
-  },
-  {
-    text: 'Act as if what you do makes a difference. It does.',
-    author: 'William James',
-  },
-  {
-    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
-    author: 'Winston Churchill',
-  },
-  {
-    text: 'The only way to do great work is to love what you do.',
-    author: 'Steve Jobs',
-  },
-  {
-    text: 'You miss 100% of the shots you don’t take.',
-    author: 'Wayne Gretzky',
-  },
-  {
-    text: 'It does not matter how slowly you go as long as you do not stop.',
-    author: 'Confucius',
-  },
-  {
-    text: 'The best way to predict the future is to create it.',
-    author: 'Peter Drucker',
-  },
-  {
-    text: 'Do what you can, with what you have, where you are.',
-    author: 'Theodore Roosevelt',
-  },
-  {
-    text: "I have not failed. I've just found 10,000 ways that won't work.",
-    author: 'Thomas Edison',
-  },
-  {
-    text: "Life is what happens when you're busy making other plans.",
-    author: 'John Lennon',
-  },
-];
+import quotes from './quotes.js';
 
 const quoteElement = document.getElementById('quote-text');
 const quoteAuthorElement = document.getElementById('author');
 const quoteBtn = document.getElementById('new-quote');
+const toggleFavoriteBtn = document.getElementById('toggle-favorite');
+const favoritesContainer = document.getElementById('favorite-container');
 
-let index;
+let currentIndex = -1;
 function getRandomQuote() {
-  let randomIndex = Math.floor(Math.random() * quotes.length);
-  while (randomIndex === index) {
+  let randomIndex;
+  do {
     randomIndex = Math.floor(Math.random() * quotes.length);
-  }
-  index = randomIndex;
+  } while (randomIndex === currentIndex); // Повторяем, пока индекс совпадает
+  currentIndex = randomIndex; // Обновляем текущий индекс
   const quote = quotes[randomIndex];
   const { text: quoteText, author: quoteAuthorText } = quote;
   quoteElement.textContent = quoteText;
   quoteAuthorElement.textContent = quoteAuthorText;
+
+  toggleFavoriteBtn.textContent = quote.isFavorite
+    ? 'Remove from favorite'
+    : 'Add to favorite';
+
+  toggleFavoriteBtn.style.display = 'inline-block';
+}
+
+function toggleFavorite() {
+  const quote = quotes[currentIndex];
+  quote.isFavorite = !quote.isFavorite;
+  toggleFavoriteBtn.textContent = quote.isFavorite
+    ? 'Remove from favorite'
+    : 'Add to favorite';
+
+  if (quote.isFavorite) {
+    const favoriteCardElement = document.createElement('div');
+    favoriteCardElement.classList.add('favoriteCard');
+    favoriteCardElement.innerHTML = `<p>${quote.text}</p>
+    <p class='author'> ${quote.author}</p>`;
+    favoritesContainer.appendChild(favoriteCardElement);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favoriteCard');
+    console.log(favoriteCards);
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(quote.text)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 const hiFunc = () => console.log('Surprise my boy');
 quoteElement.addEventListener('click', hiFunc);
 
 quoteBtn.addEventListener('click', getRandomQuote);
-
-// Load the first quote
-// getRandomQuote();
+toggleFavoriteBtn.addEventListener('click', toggleFavorite);
